d4a421b456e7df0f1ea9ed8da4c79b9de2164692
bwegener <wegener_brian@yahoo.com>
2020-06-06 13:03:22 -0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Brian Wegener on 6/6/20.
+//  Copyright © 2020 Brian Wegener. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
